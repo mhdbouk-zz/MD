862f503bf180be1af7871e39248fdd538433756c
Mohamad Dbouk <mdbouk@bi.care>
2017-09-05 13:32:06 +0300

    First commit ExtensionMethods class

---
+++ ExtensionMethods.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace MD.Extension
+{
+    public static class ExtensionMethods 
+    {
+
+    }
+}
