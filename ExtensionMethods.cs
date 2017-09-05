9f0d3f06c4883af10368bdf8ceda3b3c3e843934
Mohamad Dbouk <mdbouk@bi.care>
2017-09-05 13:34:37 +0300

    Added ToBool extension

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -8,8 +8,21 @@ using System.Xml.Serialization;
 
 namespace MD.Extension
 {
+    /// <summary>
+    /// This class is responsible of all extension methods for the application
+    /// </summary>
     public static class ExtensionMethods 
     {
-
+        /// <summary>
+        /// Tries to convert the specified string representation of a logical value to its
+        /// System.Boolean equivalent. A return value indicates the boolean value of the string or false if the convertion failed.
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns>boolean value if self was converted successfully; otherwise, false</returns>
+        public static bool ToBool(this string self)
+        {
+            bool.TryParse(self, out bool result);
+            return result;
+        }
     }
 }
